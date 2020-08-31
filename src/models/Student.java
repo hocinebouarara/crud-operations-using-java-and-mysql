6cb6e256adae40c3063460325a86de7fa642f7bb
hocinebouarara <hocinbouarara08@gmail.com>
2020-08-31 14:56:49 +0100

    create class java class of student model

---
+++ src/models/Student.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author hocin
+ */
+public class Student {
+    
+    
+    int id  ;
+    String name ;
+    Date birth ;
+    String adress ,email ;
+
+    public Student(int id, String name, Date birth, String adress, String email) {
+        this.id = id;
+        this.name = name;
+        this.birth = birth;
+        this.adress = adress;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+}
