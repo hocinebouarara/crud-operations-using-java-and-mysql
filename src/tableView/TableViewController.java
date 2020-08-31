54439f5e71f7f9f07191cdb250052c092a3e6acb
hocinebouarara <hocinbouarara08@gmail.com>
2020-08-31 15:00:52 +0100

    this class it is controller of table view content load & delete methods

---
+++ src/tableView/TableViewController.java
@@ -0,0 +1,244 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tableView;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import helpers.DbConnect;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Observable;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Callback;
+import jdk.nashorn.internal.objects.annotations.Property;
+import models.Student;
+
+/**
+ * FXML Controller class
+ *
+ * @author hocin
+ */
+public class TableViewController implements Initializable {
+
+    @FXML
+    private TableView<Student> studentsTable;
+    @FXML
+    private TableColumn<Student, String> idCol;
+    @FXML
+    private TableColumn<Student, String> nameCol;
+    @FXML
+    private TableColumn<Student, String> birthCol;
+    @FXML
+    private TableColumn<Student, String> adressCol;
+    @FXML
+    private TableColumn<Student, String> emailCol;
+    @FXML
+    private TableColumn<Student, String> editCol;
+    
+    String query = null;
+    Connection connection = null ;
+    PreparedStatement preparedStatement = null ;
+    ResultSet resultSet = null ;
+    Student student = null ;
+    
+    ObservableList<Student>  StudentList = FXCollections.observableArrayList();
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        loadDate();
+    }    
+    
+    
+    
+
+    @FXML
+    private void close(MouseEvent event) {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void getAddView(MouseEvent event) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/tableView/addStudent.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(TableViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    @FXML
+    private void refreshTable() {
+        try {
+            StudentList.clear();
+            
+            query = "SELECT * FROM `student`";
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+            
+            while (resultSet.next()){
+                StudentList.add(new  Student(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getDate("birth"),
+                        resultSet.getString("adress"),
+                        resultSet.getString("email")));
+                studentsTable.setItems(StudentList);
+                
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TableViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+    }
+
+    @FXML
+    private void print(MouseEvent event) {
+    }
+
+    private void loadDate() {
+        
+        connection = DbConnect.getConnect();
+        refreshTable();
+        
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        birthCol.setCellValueFactory(new PropertyValueFactory<>("birth"));
+        adressCol.setCellValueFactory(new PropertyValueFactory<>("adress"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+        
+        //add cell of button edit 
+         Callback<TableColumn<Student, String>, TableCell<Student, String>> cellFoctory = (TableColumn<Student, String> param) -> {
+            // make cell containing buttons
+            final TableCell<Student, String> cell = new TableCell<Student, String>() {
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    //that cell created only on non-empty rows
+                    if (empty) {
+                        setGraphic(null);
+                        setText(null);
+
+                    } else {
+
+                        FontAwesomeIconView deleteIcon = new FontAwesomeIconView(FontAwesomeIcon.TRASH);
+                        FontAwesomeIconView editIcon = new FontAwesomeIconView(FontAwesomeIcon.PENCIL_SQUARE);
+
+                        deleteIcon.setStyle(
+                                " -fx-cursor: hand ;"
+                                + "-glyph-size:28px;"
+                                + "-fx-fill:#ff1744;"
+                        );
+                        editIcon.setStyle(
+                                " -fx-cursor: hand ;"
+                                + "-glyph-size:28px;"
+                                + "-fx-fill:#00E676;"
+                        );
+                        deleteIcon.setOnMouseClicked((MouseEvent event) -> {
+                            
+                            try {
+                                student = studentsTable.getSelectionModel().getSelectedItem();
+                                query = "DELETE FROM `student` WHERE id  ="+student.getId();
+                                connection = DbConnect.getConnect();
+                                preparedStatement = connection.prepareStatement(query);
+                                preparedStatement.execute();
+                                refreshTable();
+                                
+                            } catch (SQLException ex) {
+                                Logger.getLogger(TableViewController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            
+                           
+
+                          
+
+                        });
+                        editIcon.setOnMouseClicked((MouseEvent event) -> {
+                            
+                            student = studentsTable.getSelectionModel().getSelectedItem();
+                            FXMLLoader loader = new FXMLLoader ();
+                            loader.setLocation(getClass().getResource("/tableView/addStudent.fxml"));
+                            try {
+                                loader.load();
+                            } catch (IOException ex) {
+                                Logger.getLogger(TableViewController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            
+                            AddStudentController addStudentController = loader.getController();
+                            addStudentController.setUpdate(true);
+                            addStudentController.setTextField(student.getId(), student.getName(), 
+                                    student.getBirth().toLocalDate(),student.getAdress(), student.getEmail());
+                            Parent parent = loader.getRoot();
+                            Stage stage = new Stage();
+                            stage.setScene(new Scene(parent));
+                            stage.initStyle(StageStyle.UTILITY);
+                            stage.show();
+                            
+
+                           
+
+                        });
+
+                        HBox managebtn = new HBox(editIcon, deleteIcon);
+                        managebtn.setStyle("-fx-alignment:center");
+                        HBox.setMargin(deleteIcon, new Insets(2, 2, 0, 3));
+                        HBox.setMargin(editIcon, new Insets(2, 3, 0, 2));
+
+                        setGraphic(managebtn);
+
+                        setText(null);
+
+                    }
+                }
+
+            };
+
+            return cell;
+        };
+         editCol.setCellFactory(cellFoctory);
+         studentsTable.setItems(StudentList);
+         
+         
+    }
+    
+}
